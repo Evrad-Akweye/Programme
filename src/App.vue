24335afd6470e597256692469cd9966a26da02ca
Evrad-Akweye <evradroussel@gmail.com>
2023-07-06 23:21:07 +0100

    login-page

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <LoginPage/>
+  </div>
+</template>
+
+<script>
+import LoginPage from './components/LoginPage.vue'
+export default {
+  name: 'App',
+  components: {
+    LoginPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="login">
+        <div class="Connexion"><i><b>CONNEXION</b></i></div>
+        <div class="login1">
+            <p>Login<span>*</span></p>
+            <input type="text" class="input1" placeholder="NOM">
+        </div>
+        <a href="#" >Mot de passe oublié ?</a> 
+        <div class="motdepasse">
+          <p>Mot de passe<span>*</span></p>
+            <input type="password" class="input2" placeholder="PASSWORD" >
+        </div>
+        <div class=btn>
+            <input type="submit" class="input3" value="VALIDER">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'LoginPageComponent',
+  props: {
+    
+  }
+}
+</script>
+<style scoped>
+      .login
+      {
+          background-color: antiquewhite;
+          margin: 150px;
+          height: 400px;
+          width: 450px;
+          border-radius:30px ;
+          box-shadow: 0px 0px 10px 10px black;
+          margin-left: 450px;
+          margin-bottom: 50px;
+      }
+      .input1,.input2
+      {
+          width:420px;
+          height: 50px;
+          margin-left: 10px;
+          margin-right: 10px;
+          background-color: burlywood;
+          font-size: 25px;
+        }
+        .Connexion
+        {
+            text-align: center;
+            font-size: 3em;
+            color: green;
+        }
+        a
+        {
+            text-align:end;
+            margin-top: 5px;
+            margin-right: 10px;
+            margin-left: 280px;
+        }
+        .input3
+        {
+            width: 150px;
+            height: 40px;
+            background-color: yellowgreen;
+            margin-inline-start: 150px;
+            border-radius: 10px;
+            margin-top: 40px;
+        }
+        span
+        {
+          color: red;
+        }
+        p
+        {
+            font-size: 1.5em;
+            margin-left: 10px;
+            margin-bottom: 5px;
+        }
+        .login1
+        {
+            margin-top:10px ;
+        }
+        .motdepasse
+        {
+            margin-top: 10px;
+        }
+        
+        
+</style>
